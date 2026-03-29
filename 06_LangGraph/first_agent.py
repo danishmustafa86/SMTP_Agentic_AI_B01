b9b59dd1e0e1dd6ddd1f8ea1f9f7b09023d3600a
Danish Mustafa <danishjajja86@gmail.com>
2026-03-29 20:27:15 +0500

    langgraph first workflow

---
+++ 06_LangGraph/first_agent.py
@@ -0,0 +1,44 @@
+from langgraph.graph import StateGraph, START, END
+from pydantic import BaseModel 
+import random 
+from typing import Literal 
+
+
+class state(BaseModel):
+    graph_state: str 
+    
+
+
+def node_1(state):
+    return {"graph_state": state.graph_state + " I am"}
+
+def node_2(state):
+    return {"graph_state": state.graph_state + " Happy!"}
+
+def node_3(state):
+    return {"graph_state":state.graph_state + " Sad!"}
+
+def decide_mood(state) -> Literal["node_2", "node_3"]:
+
+    if random.random() < 0.5:
+        return "node_2"
+    return "node_3"
+
+
+
+builder = StateGraph(state)
+
+builder.add_node("node_1", node_1)
+builder.add_node("node_2", node_2)
+builder.add_node("node_3", node_3)
+
+builder.add_edge(START, "node_1")
+builder.add_conditional_edges("node_1", decide_mood)
+builder.add_edge("node_2", END)
+builder.add_edge("node_3", END)
+
+graph = builder.compile()
+
+result = graph.invoke({"graph_state": "Hi, how are you?"})
+
+print(result)
