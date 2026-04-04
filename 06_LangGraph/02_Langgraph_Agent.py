f4283da93a7aaac061ccabe279d2395c140947ed
Danish Mustafa <danishjajja86@gmail.com>
2026-04-04 16:02:12 +0500

    langgraph with llm integration

--- 06_LangGraph/02_Langgraph_Agent.py
+++ 06_LangGraph/02_Langgraph_Agent.py
@@ -15,11 +15,6 @@ load_dotenv()
 llm = ChatOpenAI( model="gpt-4o-mini", api_key=os.getenv("AIMLAPI_API_KEY"), base_url="https://api.aimlapi.com/v1")
 
 
-messages = [AIMessage(content=f"So you said you were researching ocean mammals?", name="Model")]
-
-messages.append(HumanMessage(content=f"Yes, that's right.",name="Lance"))
-messages.append(AIMessage(content=f"Great, what would you like to learn about.", name="Model"))
-messages.append(HumanMessage(content=f"I want to learn about the best place to see Orcas in the US.", name="Lance"))
 
 
 def multiply(a: int, b: int) -> int:
